2081ed0b8e80836d5b39ca3975e1fcf27cffe4bc
Oleg <olegys93@gmail.com>
2016-07-06 14:42:34 +0300

    Add files via upload

--- mySiteAdaptive/js/script.js
+++ mySiteAdaptive/js/script.js
@@ -1,12 +1,25 @@
-$(document).ready(function() {
-	$('a[href*=#]').bind("click", function(e){
-		var anchor = $(this);
-			var name = anchor.attr('href').replace(new RegExp("#",'gi'), "");
-				$('html, body').stop().animate({
-					scrollTop: $('a[name='+name+']').offset().top
-				}, 1000);
-			e.preventDefault();
+// скролл по ссылкам <a> <--> <a>
+// $(document).ready(function() {
+// 	$('a[href*=#]').bind("click", function(e){
+// 		var anchor = $(this);
+// 			var name = anchor.attr('href').replace(new RegExp("#",'gi'), "");
+// 				$('html, body').stop().animate({
+// 					scrollTop: $('a[name='+name+']').offset().top
+// 				}, 1000);
+// 			e.preventDefault();
+// 		return false;
+// 	});
+// });
+
+$(document).ready(function(){
+	// Плавный скролл по якорям id
+	$('a[href^="#"]').click(function () { 
+		elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		if($.browser.safari){
+			$('body').animate( { scrollTop: destination }, 1000 );
+			} else {
+		$('html').animate( { scrollTop: destination }, 1000 );}
 		return false;
 	});
 });
-
