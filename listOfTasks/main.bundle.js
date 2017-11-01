15fa4e46087a49bae34d363c54345a5b28dda7f2
Oleg <olegys93@gmail.com>
2017-11-01 19:05:20 +0200

    add task list

--- listOfTasks/main.bundle.js
+++ listOfTasks/main.bundle.js
@@ -58,7 +58,7 @@ var AppRoutingModule = (function () {
 }());
 AppRoutingModule = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["M" /* NgModule */])({
-        imports: [__WEBPACK_IMPORTED_MODULE_1__angular_router__["c" /* RouterModule */].forRoot(routes)],
+        imports: [__WEBPACK_IMPORTED_MODULE_1__angular_router__["c" /* RouterModule */].forRoot(routes, { useHash: true })],
         exports: [__WEBPACK_IMPORTED_MODULE_1__angular_router__["c" /* RouterModule */]]
     })
 ], AppRoutingModule);
